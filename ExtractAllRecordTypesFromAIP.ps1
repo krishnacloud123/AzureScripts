e6e36dae3840647f57a629d80243008ec09c1300
krishna <66212565+krishnacloud123@users.noreply.github.com>
2022-10-06 11:47:37 -0400

    Update ExtractAllRecordTypesFromAIP.ps1

--- ExtractAllRecordTypesFromAIP.ps1
+++ ExtractAllRecordTypesFromAIP.ps1
@@ -1,3 +1,32 @@
+<#   
+The MIT License (MIT)
+Copyright (c) 2015 Microsoft Corporation
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+
+
+<#
+Script      : ExtractAllRecordTypesFromAIP.PS1
+Author      : Krishna V
+Version     : 1.0.0
+Description : The script exports data from  Search-UnifiedAuditLog to csv file for all the events and also publishes total time for the query to extract the data.
+#>
+
 $sessionID = [Guid]::NewGuid().ToString() + "_" +  "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
 [DateTime]$start = [DateTime]::UtcNow.Adddays(-21)
 [DateTime]$end = [DateTime]::UtcNow
@@ -12,19 +41,9 @@ Import-Module ExchangeOnlineManagement
 
 #connect to exchangeonline
 Connect-ExchangeOnline
+ 
 
-	
-
-#93	AipDiscover	Azure Information Protection (AIP) scanner events.
-#94	AipSensitivityLabelAction	AIP sensitivity label events.
-#95	AipProtectionAction	AIP protection events.
-#96	AipFileDeleted	AIP file deletion events.
-#97	AipHeartBeat	AIP heartbeat events.
-
-
-#$record = @('AipSensitivityLabelAction', 'AipProtectionAction', 'AipFileDeleted', 'AipHeartBeat','AipDiscover')
-#$record = 'AipDiscover'
-
+ 
 # The same script can be extened to include other records 
 $record = "ExchangeAdmin","ExchangeItem","ExchangeItemGroup","SharePoint","SyntheticProbe","SharePointFileOperation","OneDrive","AzureActiveDirectory","AzureActiveDirectoryAccountLogon","DataCenterSecurityCmdlet","ComplianceDLPSharePoint","Sway","ComplianceDLPExchange","SharePointSharingOperation","AzureActiveDirectoryStsLogon","SkypeForBusinessPSTNUsage","SkypeForBusinessUsersBlocked","SecurityComplianceCenterEOPCmdlet","ExchangeAggregatedOperation","PowerBIAudit","CRM","Yammer","SkypeForBusinessCmdlets","Discovery","MicrosoftTeams","ThreatIntelligence","MailSubmission","MicrosoftFlow","AeD","MicrosoftStream","ComplianceDLPSharePointClassification","ThreatFinder","Project","SharePointListOperation","SharePointCommentOperation","DataGovernance","Kaizala","SecurityComplianceAlerts","ThreatIntelligenceUrl","SecurityComplianceInsights","MIPLabel","WorkplaceAnalytics","PowerAppsApp","PowerAppsPlan","ThreatIntelligenceAtpContent","LabelContentExplorer","TeamsHealthcare","ExchangeItemAggregated","HygieneEvent","DataInsightsRestApiAudit","InformationBarrierPolicyApplication","SharePointListItemOperation","SharePointContentTypeOperation","SharePointFieldOperation","MicrosoftTeamsAdmin","HRSignal
 ","MicrosoftTeamsDevice","MicrosoftTeamsAnalytics","InformationWorkerProtection","Campaign","DLPEndpoint","AirInvestigation","Quarantine","MicrosoftForms","ApplicationAudit","ComplianceSupervisionExchange","CustomerKeyServiceEncryption","OfficeNative","MipAutoLabelSharePointItem","MipAutoLabelSharePointPolicyLocation","MicrosoftTeamsShifts","MipAutoLabelExchangeItem","CortanaBriefing","Search","WDATPAlerts","MDATPAudit"
